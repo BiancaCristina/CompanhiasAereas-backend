19b2f55e6d84238871d4464f6bbe9007aefe2fe6
BiancaCristina <biancacristina-silva@outlook.com>
2019-02-07 21:01:35 -0200

    Commit inicial

---
+++ src/main/kotlin/br/ufu/facom/bianca/CompanhiaArea/CompanhiaAreaApplication.kt
@@ -0,0 +1,12 @@
+package br.ufu.facom.bianca.CompanhiaArea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompanhiaAreaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CompanhiaAreaApplication>(*args)
+}
+
